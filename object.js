11bf3aa42f98047073ed2267324c373245e3d2f3
CTD <qpflapffhs76@gmail.com>
2021-09-13 15:20:44 +0900

    JS OOP

---
+++ object.js
@@ -0,0 +1,12 @@
+let memberArray = ["egoing", "graphittie", "leezhce"];
+console.log("memberArray[1]: " + memberArray[1]);
+
+let memberObject = {
+  manager: "egoing",
+  developer: "graphittie",
+  designer: "leezhce",
+};
+
+console.log(memberObject);
+delete memberObject.manager;
+console.log(memberObject);
---
+++ this.js
@@ -0,0 +1,24 @@
+let kim = {
+  name: "kim",
+  first: 10,
+  second: 20,
+  third: 30,
+  sum: function () {
+    return this.first + this.second + this.third;
+  },
+};
+
+let lee = {
+  name: "lee",
+  first: 10,
+  second: 10,
+  third: 10,
+
+  sum: function () {
+    return this.first + this.second;
+  },
+};
+
+console.log(kim.sum(kim.first, kim.second));
+console.log(kim.sum());
+console.log(lee.sum());
